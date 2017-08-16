b3f5e60d26f68587645a268aeef6e3f1caf427a8
nmerris <nate.merris@gmail.com>
2017-08-16 11:39:00 -0400

    disabled submit button in addeducation, starting week4project here

--- src/main/java/com/nmerris/roboresumedb/Week4ProjectApplication.java
+++ src/main/java/com/nmerris/roboresumedb/Week4ProjectApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Week3ProjectApplication {
+public class Week4ProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Week3ProjectApplication.class, args);
+		SpringApplication.run(Week4ProjectApplication.class, args);
 	}
 }
--- src/test/java/com/nmerris/roboresumedb/Week4ProjectApplicationTests.java
+++ src/test/java/com/nmerris/roboresumedb/Week4ProjectApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class Week3ProjectApplicationTests {
+public class Week4ProjectApplicationTests {
 
 	@Test
 	public void contextLoads() {
